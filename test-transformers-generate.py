a5828a13c92b29dce381cd16fcf20e167b96d8ae
A9isha <anisha.mazumder@gmail.com>
2023-04-04 17:13:53 +0000

    Push model and inputs to device

---
+++ test-transformers-generate.py
@@ -0,0 +1,35 @@
+from typing import Tuple
+import os
+import sys
+import torch
+# import fire
+import time
+import torch_xla.core.xla_model as xm
+import torch_xla.debug.metrics as met
+import torch_xla.debug.profiler as xp
+import json
+
+
+from transformers import AutoModelForCausalLM
+model = AutoModelForCausalLM.from_pretrained("gpt2")
+print("model",model)
+
+device = xm.xla_device()
+model = model.to(device)
+
+
+
+from transformers import AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained("gpt2")
+
+
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = "left"
+inputs = tokenizer(["Sky is the limit","Anisha learning"],return_tensors='pt', padding="longest")
+print(inputs)
+inputs = inputs.to(device)
+
+outputs=model.generate(**inputs, max_new_tokens=80, do_sample=False)
+print("outputs=")
+print(outputs)   
